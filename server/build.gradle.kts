4bf13c9de46845cc54d42221b036e3030f9460fe
Sam Pidia <sampidia0@gmail.com>
2025-11-14 02:05:33 +0100

    Fix Render build: add Kotlin plugin version to server module

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") version "1.9.21"
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
