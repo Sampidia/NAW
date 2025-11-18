62eb7c5e51e65b2956f6f3db4c1be0b01003da79
Sam Pidia <sampidia0@gmail.com>
2025-11-18 21:00:33 +0100

    initial fix1

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization")
 }
 
 group = "com.naijaayo"
 version = "1.0-SNAPSHOT"
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     testImplementation(kotlin("test"))
 }
 
